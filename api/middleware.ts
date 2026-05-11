bd98142c699627ded1b63806d9a094e0e66c59bc
Skill Template <template@skill>
2026-05-12 01:23:58 +0800

    Clean functions folder token in wrangler

--- api/middleware.ts
+++
@@ -1,7 +0,0 @@
-import { initTRPC } from "@trpc/server";
-import type { Context } from "./context";
-
-const t = initTRPC.context<Context>().create();
-
-export const createRouter = t.router;
-export const publicProcedure = t.procedure;
